e97f62b656b7caae5b8701c1ce5894f78e41ba0b
Andrew Willms <AndrewW@hyivy.com>
2025-04-25 17:30:55 -0400

    changed functions to trait extensions methods

--- Ledger/src/Purchase.rs
+++ Ledger/src/Purchase.rs
@@ -4,57 +4,64 @@ use chrono::{DateTime, Utc};
 use crate::{Charge, ChargeData, Expense, ExpenseData, Merchant, Transaction};
 
 pub(crate) struct Purchase<'a> {
-    pub name: String,
-    merchant: Option<&'a Merchant<'a>>,
-    date: DateTime<Utc>,
-    pub charges: Vec<Charge<'a>>,
-    expenses: Vec<Expense<'a>>,
-    associated_transactions: Vec<Transaction<'a>>
+	pub name: String,
+	merchant: Option<&'a Merchant<'a>>,
+	date: DateTime<Utc>,
+	pub charges: Vec<Charge<'a>>,
+	expenses: Vec<Expense<'a>>,
+	associated_transactions: Vec<Transaction<'a>>
 }
 
 pub fn create<'a>(name: String, merchant: Option<&'a Merchant<'a>>, date: DateTime<Utc>) -> Rc<RefCell<Purchase<'a>>> {
-
-    let purchase = Purchase {
-        name,
-        merchant,
-        date,
-        charges: vec![],
-        expenses: vec![],
-        associated_transactions: vec![],
-    };
-
-    Rc::new(RefCell::new(purchase))
+	
+	let purchase = Purchase {
+		name,
+		merchant,
+		date,
+		charges: vec![],
+		expenses: vec![],
+		associated_transactions: vec![],
+	};
+	
+	Rc::new(RefCell::new(purchase))
 }
 
-pub fn add_charge<'a>(purchase: Rc<RefCell<Purchase<'a>>>, charge_data: ChargeData<'a>) {
-
-    let shared_pointer = Rc::clone(&purchase);
-
-    purchase.borrow_mut().charges.push(Charge {
-        name: charge_data.name,
-        date_merchant_processed: charge_data.date_account_processed,
-        date_account_processed: charge_data.date_account_processed,
-        account: charge_data.account,
-        purchase: shared_pointer,
-        amount: charge_data.amount,
-    });
+pub trait PurchaseStuff {
+	fn add_charge(self, charge_data: ChargeData);
+	fn add_expense(self, expense_data: ExpenseData);
+	fn add_associated_transaction(self, transaction: Transaction);
 }
 
-pub fn add_expense<'a>(purchase: Rc<RefCell<Purchase<'a>>>, expense_data: ExpenseData<'a>) {
+impl PurchaseStuff for Rc<RefCell<Purchase<'_>>> {
+	
+	fn add_charge(self, charge_data: ChargeData) {
 
-    let shared_pointer = Rc::clone(&purchase);
+		let ref_cell_clone = Rc::clone(&self);
 
-    purchase.borrow_mut().expenses.push(Expense {
-        name: expense_data.name,
-        budget: expense_data.budget,
-        purchase: shared_pointer,
-        amount: expense_data.amount,
-    });
-}
+		self.borrow_mut().charges.push(Charge {
+			name: charge_data.name,
+			date_merchant_processed: charge_data.date_account_processed,
+			date_account_processed: charge_data.date_account_processed,
+			account: charge_data.account,
+			purchase: ref_cell_clone,
+			amount: charge_data.amount,
+		});
+	}
 
-impl<'a> Purchase<'a> {
+	fn add_expense<'a>(self, expense_data: ExpenseData<'a>) {
+		
+		let ref_cell_clone = Rc::clone(&self);
 
-    pub fn add_associated_transaction(mut self, transaction: Transaction<'a>) {
-        self.associated_transactions.push(transaction);
-    }
+		self.borrow_mut().expenses.push(Expense {
+			name: expense_data.name,
+			budget: expense_data.budget,
+			purchase: ref_cell_clone,
+			amount: expense_data.amount,
+		});
+	}
+
+	fn add_associated_transaction<'a>(self, transaction: Transaction<'a>) {
+		self.borrow_mut().associated_transactions.push(transaction);
+	}
+
 }
--- Ledger/src/main.rs
+++ Ledger/src/main.rs
@@ -2,132 +2,133 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use chrono::{DateTime, Utc};
 use rust_decimal::{Decimal};
-use crate::purchase::Purchase;
-
+use crate::purchase::{Purchase, PurchaseStuff};
 mod purchase;
 
+
 fn main() {
-    println!("Hello, world!");
+	
+	println!("Hello, world!");
+	
+	let test_purchase: Rc<RefCell<Purchase>> = purchase::create("name".to_string(), None, Utc::now());
 
-    let test_purchase = purchase::create("name".to_string(), None, Utc::now());
+	let account = Account { name: "account name".to_string(), institution: None };
+	let budget = Budget { name: "budget name".to_string() };
 
-    let account = Account { name: "account name".to_string(), institution: None };
-    let budget = Budget { name: "budget name".to_string() };
+	let charge_data = ChargeData {
+		name: "charge name".to_string(),
+		date_merchant_processed: Utc::now(),
+		date_account_processed: Utc::now(),
+		account: &account,
+		amount: Decimal::new(1, 0),
+	};
 
-    let charge_data = ChargeData {
-        name: "charge name".to_string(),
-        date_merchant_processed: Utc::now(),
-        date_account_processed: Utc::now(),
-        account: &account,
-        amount: Decimal::new(1, 0),
-    };
-
-    let expense_data = ExpenseData {
-        name: "".to_string(),
-        budget: &budget,
-        amount: Decimal::new(1, 0),
-    };
-
-    purchase::add_charge(test_purchase.clone(), charge_data);
-    purchase::add_expense(test_purchase.clone(), expense_data);
+	let expense_data = ExpenseData {
+		name: "".to_string(),
+		budget: &budget,
+		amount: Decimal::new(1, 0),
+	};
+	
+	test_purchase.clone().add_charge(charge_data);
+	test_purchase.clone().add_expense(expense_data);
 }
 
 struct Institution {
-    name: String
+	name: String
 }
 
 struct Account<'a> {
-    name: String,
-    institution: Option<&'a Institution>,
+	name: String,
+	institution: Option<&'a Institution>,
 }
 
 struct Budget {
-    name: String,
+	name: String,
 }
 
 struct MerchantFamily {
-    name: String,
+	name: String,
 }
 
 struct Merchant<'a> {
-    name: String,
-    merchant_family: Option<&'a MerchantFamily>,
-    address: Option<String>,
+	name: String,
+	merchant_family: Option<&'a MerchantFamily>,
+	address: Option<String>,
 }
 
 
 
 
 struct Charge<'a> {
-    name: String,
-    date_merchant_processed: DateTime<Utc>,
-    date_account_processed: DateTime<Utc>,
-    account: &'a Account<'a>,
-    purchase: Rc<RefCell<Purchase<'a>>>,
-    amount: Decimal,
+	name: String,
+	date_merchant_processed: DateTime<Utc>,
+	date_account_processed: DateTime<Utc>,
+	account: &'a Account<'a>,
+	purchase: Rc<RefCell<Purchase<'a>>>,
+	amount: Decimal,
 }
 
 struct ChargeData<'a> {
-    name: String,
-    date_merchant_processed: DateTime<Utc>,
-    date_account_processed: DateTime<Utc>,
-    account: &'a Account<'a>,
-    amount: Decimal,
+	name: String,
+	date_merchant_processed: DateTime<Utc>,
+	date_account_processed: DateTime<Utc>,
+	account: &'a Account<'a>,
+	amount: Decimal,
 }
 
 struct Expense<'a> {
-    name: String,
-    budget: &'a Budget,
-    purchase: Rc<RefCell<Purchase<'a>>>,
-    amount: Decimal,
+	name: String,
+	budget: &'a Budget,
+	purchase: Rc<RefCell<Purchase<'a>>>,
+	amount: Decimal,
 }
 
 struct ExpenseData<'a> {
-    name: String,
-    budget: &'a Budget,
-    amount: Decimal,
+	name: String,
+	budget: &'a Budget,
+	amount: Decimal,
 }
 
 struct Item<'a> {
-    name: String,
-    notes: String,
-    purchase: Purchase<'a>,
+	name: String,
+	notes: String,
+	purchase: Purchase<'a>,
 
-    date_order_processed: Option<DateTime<Utc>>,
-    date_shipped: Option<DateTime<Utc>>,
-    date_received: Option<DateTime<Utc>>,
+	date_order_processed: Option<DateTime<Utc>>,
+	date_shipped: Option<DateTime<Utc>>,
+	date_received: Option<DateTime<Utc>>,
 
-    regular_price: Decimal,
-    sale_price: Decimal,
-    effective_price: Decimal,
+	regular_price: Decimal,
+	sale_price: Decimal,
+	effective_price: Decimal,
 }
 
 struct Transfer<'a> {
-    name: String,
-    date_initiated: DateTime<Utc>,
-    date_sent: DateTime<Utc>,
-    date_received: DateTime<Utc>,
-    source_account: &'a Account<'a>,
-    destination_account: &'a Account<'a>,
-    amount: Decimal
+	name: String,
+	date_initiated: DateTime<Utc>,
+	date_sent: DateTime<Utc>,
+	date_received: DateTime<Utc>,
+	source_account: &'a Account<'a>,
+	destination_account: &'a Account<'a>,
+	amount: Decimal
 }
 
 struct TransferBudget {
-    name: String,
-    date: DateTime<Utc>,
-    source_budget: Budget,
-    destination_budget: Budget,
-    amount: Decimal,
+	name: String,
+	date: DateTime<Utc>,
+	source_budget: Budget,
+	destination_budget: Budget,
+	amount: Decimal,
 }
 
 struct Income<'a> {
-    name: String,
-    account: &'a Account<'a>,
-    date: DateTime<Utc>,
-    amount: Decimal
+	name: String,
+	account: &'a Account<'a>,
+	date: DateTime<Utc>,
+	amount: Decimal
 }
 
 enum Transaction<'a> {
-    Income(Income<'a>),
-    Expense(Expense<'a>),
+	Income(Income<'a>),
+	Expense(Expense<'a>),
 }
